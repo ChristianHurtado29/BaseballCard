744e2e80fa324b8d92fb0a215e1cce1c1967e171
Christian Hurtado <christian.hurtado.catacoli@gmail.com>
2020-07-22 12:30:05 -0400

    Initial Commit

---
+++ BaseballCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseballCard
+//
+//  Created by Christian Hurtado on 7/22/20.
+//  Copyright © 2020 Christian Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
